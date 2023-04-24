4abd91e882464c8b4560e99e795fefd5a9d99574
Ari-Ario <xusrew.z@gmail.com>
2023-04-24 17:49:44 +0200

    pad  structure
    first window creatad

--- pad.py
+++ pad.py
@@ -1,1 +1,12 @@
-print("hallo")
+"""A simple pad with diffrent function"""
+
+import tkinter
+from tkinter import *
+from tkinter import ttk, messagebox, colorchooser
+
+root = Tk()
+root.title("PADY")
+c = Canvas(root, background="light cyan")
+c.grid(row=0, column=0)
+
+root.mainloop()
