04d79aa68902e1c1338159bd3a71a382f9d3f27c
Ari-Ario <xusrew.z@gmail.com>
2023-04-25 06:45:44 +0200

    size of the pad changed

--- pad.py
+++ pad.py
@@ -6,7 +6,10 @@ from tkinter import ttk, messagebox, colorchooser
 
 root = Tk()
 root.title("PADY")
-c = Canvas(root, background="light cyan")
+root.geometry("700x500")
+l_frame= LabelFrame(root, text="Draw on the pad")
+l_frame.grid(row=0, column=0)
+c = Canvas(l_frame, background="light cyan", width=700, height=450)
 c.grid(row=0, column=0)
 
 root.mainloop()
