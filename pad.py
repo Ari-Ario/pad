523c158479b32c4ea610a49937251a941cd34437
Ari-Ario <xusrew.z@gmail.com>
2023-04-25 15:06:20 +0200

    layout changed

--- pad.py
+++ pad.py
@@ -4,12 +4,39 @@ import tkinter
 from tkinter import *
 from tkinter import ttk, messagebox, colorchooser
 
-root = Tk()
-root.title("PADY")
-root.geometry("700x500")
-l_frame= LabelFrame(root, text="Draw on the pad")
-l_frame.grid(row=0, column=0)
-c = Canvas(l_frame, background="light cyan", width=700, height=450)
-c.grid(row=0, column=0)
+#in oop: class instead functional pro.
+class main:
+    def __init__(self, master):
+        self.master = master
+        self.col_fg = "black"
+        self.col_bg = "cyan"
+        self.pre_x = None
+        self.pre_y = None
+        self.brush_width= 5
+        self.draw_widgets()
+    
+    def paint(self):
+        pass
 
-root.mainloop()
+    def draw_widgets(self):
+        self.controls = Frame(self.master, padx=5, pady=5)
+        textpw = Label(self.controls, text="Pen Width")
+        textpw.grid(row=0, column=0)
+        self.slider= ttk.Scale(self.controls, from_=5, to=100, command=None)
+        self.slider.set(self.brush_width)
+        self.slider.grid(row=1, column=0)
+        self.controls.pack(side=LEFT)
+        self.c = Canvas(self.master, bg=self.col_bg)
+        self.c.pack(fill=BOTH, expand=True)
+        #menu
+        menubar = Menu(self.master)
+        self.master.config(menu=menubar)
+        filemeu= Menu(menubar)
+        menubar.add_cascade(label="Menu", menu=filemeu)
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("PADY")
+    main(root)
+    root.mainloop()
