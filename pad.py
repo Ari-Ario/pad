f5ca1bd7838d2c19bbd8a0b06849c3c9eff68b9f
Ari-Ario <xusrew.z@gmail.com>
2023-04-27 08:07:48 +0200

    profetional methods add
    methods of rectangle and triangle added, plus a popupmenu

--- pad.py
+++ pad.py
@@ -9,7 +9,7 @@ class main:
     def __init__(self, master):
         self.master = master
         self.col_fg = "black"
-        self.col_bg = "cyan"
+        self.col_bg = "light cyan"
         self.pre_x = None
         self.pre_y = None
         self.brush_width= 5
@@ -56,29 +56,31 @@ class main:
 
     #method to creat a line
     def coordinates_win(self):
-        self.win_line = Tk()
-        self.win_line.mainloop
-        self.win_line.title("Coordinates")
-        label_x1 = Label(self.win_line, text="X1 = ", width=10).grid(row=0, column=0)
-        label_y1 = Label(self.win_line, text="Y1 = ", width=10).grid(row=1, column=0)
-        label_x2 = Label(self.win_line, text="X2 = ", width=10).grid(row=2, column=0)
-        label_y2 = Label(self.win_line, text="Y2 = ", width=10).grid(row=3, column=0)
-        self.entry_x1 = Entry(self.win_line, width=10)
+        self.win_coor = Tk()
+        self.win_coor.mainloop
+        self.win_coor.title("Coordinates")
+        label_x1 = Label(self.win_coor, text="   X1 --\N{RIGHTWARDS ARROW}", width=10).grid(row=0, column=0)
+        label_y1 = Label(self.win_coor, text="   Y1 --\N{RIGHTWARDS ARROW}", width=10).grid(row=1, column=0)
+        label_x2 = Label(self.win_coor, text="   X2 --\N{RIGHTWARDS ARROW}", width=10).grid(row=2, column=0)
+        label_y2 = Label(self.win_coor, text="   Y2 --\N{RIGHTWARDS ARROW}", width=10).grid(row=3, column=0)
+        self.entry_x1 = Entry(self.win_coor, width=10)
         self.entry_x1.grid(row=0, column=1)
-        self.entry_y1 = Entry(self.win_line, width=10)
+        self.entry_y1 = Entry(self.win_coor, width=10)
         self.entry_y1.grid(row=1, column=1)
-        self.entry_x2 = Entry(self.win_line, width=10)
+        self.entry_x2 = Entry(self.win_coor, width=10)
         self.entry_x2.grid(row=2, column=1)
-        self.entry_y2 = Entry(self.win_line, width=10)
+        self.entry_y2 = Entry(self.win_coor, width=10)
         self.entry_y2.grid(row=3, column=1)
         #two buttons, which splits the functionality of inserting line and oval by licking on them
-        but_insert_line= Button(self.win_line, text="Insert line", relief=RAISED, command= self.get_line)
+        but_insert_line= Button(self.win_coor, text="Insert line", relief=RAISED, command= self.get_line)
         but_insert_line.grid(row=4, column=0, sticky=NSEW)
-        but_insert_oval= Button(self.win_line, text="Insert Oval", relief=RAISED, command= self.get_oval)
+        but_insert_oval= Button(self.win_coor, text="Insert Oval", relief=RAISED, command= self.get_oval)
         but_insert_oval.grid(row=4, column=1, sticky=NSEW)
+        but_insert_rectangle= Button(self.win_coor, text="Insert Rectangle", relief=RAISED, command=self.get_rect)
+        but_insert_rectangle.grid(row=5, column=0, sticky=NSEW)
 
-        but_cancel= Button(self.win_line, text="Quit", relief=RAISED, command=self.win_line.destroy)
-        but_cancel.grid(row=5, column=0, columnspan=2, sticky=NSEW)
+        but_cancel= Button(self.win_coor, text="Quit\N{RIGHTWARDS ARROW}", fg="red", relief=RAISED, command=self.win_coor.destroy)
+        but_cancel.grid(row=5, column=1, columnspan=1, sticky=NSEW)
 
     #this method is bound with the button insert as |
     def get_line(self):
@@ -88,10 +90,10 @@ class main:
         self.y2= self.entry_y2.get()
         if self.x1 and self.x2 and self.y1 and self.y2:
             self.c.create_line(self.x1, self.y1, self.x2, self.y2, fill=self.col_fg, width=self.brush_width)
-            self.win_line.destroy()
+            self.win_coor.destroy()
             self.reset("<ButtonRelease-1>")
         else:
-            messagebox.showerror("Error", "please fill in all coordinates or cancel for exit.")
+            messagebox.showerror("Error", "please fill in all coordinates or press Quit.")
     #this method is bound with the button insert as O
     def get_oval(self):
         self.x1= self.entry_x1.get()
@@ -100,10 +102,30 @@ class main:
         self.y2= self.entry_y2.get()
         if self.x1 and self.x2 and self.y1 and self.y2:
             self.c.create_oval(self.x1, self.y1, self.x2, self.y2, fill=self.col_fg, offset="nw", width=self.brush_width)
-            self.win_line.destroy()
+            self.win_coor.destroy()
             self.reset("<ButtonRelease-1>")
         else:
-            messagebox.showerror("Error", "please fill in all coordinates or cancel for exit.")
+            messagebox.showerror("Error", "please fill in all coordinates or press Quit.")
+
+    #method to get rectangle
+    def get_rect(self):
+        self.x1= self.entry_x1.get()
+        self.y1= self.entry_y1.get()
+        self.x2= self.entry_x2.get()
+        self.y2= self.entry_y2.get()
+        if self.x1 and self.x2 and self.y1 and self.y2:
+            self.c.create_rectangle(self.x1, self.y1, self.x2, self.y2, fill=self.col_fg, offset="nw", width=self.brush_width)
+            self.win_coor.destroy()
+            self.reset("<ButtonRelease-1>")
+        else:
+            messagebox.showerror("Error", "please fill in all coordinates or press Quit.")
+
+    #method of popup-creator
+    def do_popup(self, event):
+        try:
+            self.popup_menu.tk_popup(event.x_root, event.y_root, 0)
+        finally:
+            self.popup_menu.grab_release()
 
 
     #all labels, frames, canvas, filemenu, etc. within this method
@@ -137,7 +159,16 @@ class main:
         menubar.add_cascade(label="Insert", menu=geometrymenu)
         geometrymenu.add_command(label="Line" , command=self.coordinates_win)
         geometrymenu.add_command(label="Circle" , command=self.coordinates_win)
-        geometrymenu.add_command(label="Triangular" , command=None)
+        geometrymenu.add_command(label="Rectangle" , command=self.coordinates_win)
+        geometrymenu.add_separator()
+        geometrymenu.add_command(label="Triangular", command=None) #must have another window
+        #pop-up menu by right-click
+        self.popup_menu = Menu(self.c, tearoff=0)
+        #copy menu will be added
+        #cut menu will be added
+        self.popup_menu.add_command(label="Insert shape", command=self.coordinates_win)
+        #binding the popup menu with its method above: do_popup
+        self.c.bind("<Button-3>", self.do_popup)
 
 
 if __name__ == "__main__":
